408ef5539782dd4d9c117baa862c6989ed2cdaee
Joseph Case <joseph.case@hotmail.co.uk>
2018-02-05 17:16:51 +0100

    Created RomanNumber class with dummy function to test working

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # RomanNumeralsChallenge
 
 START TIME: 4:47 PM Monday, February 5, 2018 (GMT+1)
+
+
+#### N.B. Running with Node v8.9.4
--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+// tests go here
+
+const RomanNumber = require('./RomanNumber');
+
 console.log('Hello world!');
+
+const romanNumber1 = new RomanNumber('any input here');
+
+console.log(romanNumber1.getInput());
+
